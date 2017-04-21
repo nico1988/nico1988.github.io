82a146b8254cae3cd0b58caae4e6034ef163746d
nico0618 <410827024@qq.com>
2017-04-21 22:35:48 +0800

    Site updated: 2017-04-21 22:35:47

--- js/gallery.js
+++ js/gallery.js
@@ -79,6 +79,8 @@
   var div_aside = $('<div class="touch_out"></div>');
   $(div_aside).html(nav_html).css({"position":"fixed","left":"-225px","top":$("body>header")[0].offsetHeight,"zIndex":30,"background":"rgba(135,206,235,.9)","height":"100%"}).find("li").css({"padding":"15px","borderBottom":"1px solid pink"});
   $("body").append(div_aside);
+  var left = $(".touch_out").width()+"px";
+  $("div.touch_out").css({"left":-left});
   touch();//触摸屏幕显示侧边栏
   function touch(event){
       //算的时候一定要带单位
